6cb364654733b1e9df0106dc7f49bbc87a5416c4
mouniudayraj-cyber <mouniudayraj@gmail.com>
2026-06-14 02:10:37 +0530

    Update route.js

--- app/api/auth/angelone/route.js
+++ app/api/auth/angelone/route.js
@@ -1,5 +1,5 @@
 export async function GET() {
   return Response.redirect(
-    "https://smartapi.angelone.in/publisher-login?api_key=YOUR_API_KEY"
+    "https://smartapi.angelone.in/publisher-login?api_key=983mzMi0"
   );
 }
