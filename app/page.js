27357157f8037ace75b184126dacffc312287cf4
mouniudayraj-cyber <mouniudayraj@gmail.com>
2026-06-14 02:11:42 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,20 +1,3 @@
-export default function Home() {
-  return (
-    <main style={{
-      background:"#0f172a",
-      color:"white",
-      minHeight:"100vh",
-      padding:"40px"
-    }}>
-      <h1>🚀 MarketBrain</h1>
-      <h2>Live Market Dashboard</h2>
-
-      <p>NIFTY : Loading...</p>
-      <p>BANKNIFTY : Loading...</p>
-
-      <button>
-        Connect Angel One
-      </button>
-    </main>
-  )
-}
+<a href="/api/auth/angelone">
+  <button>Connect Angel One</button>
+</a>
