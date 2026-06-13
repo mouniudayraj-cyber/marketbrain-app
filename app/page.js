3db232f75453e28dce8fd8827c0234ee8393276d
mouniudayraj-cyber <mouniudayraj@gmail.com>
2026-06-14 01:25:17 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <main style={{
+      background:"#0f172a",
+      color:"white",
+      minHeight:"100vh",
+      padding:"40px"
+    }}>
+      <h1>🚀 MarketBrain</h1>
+
+      <h2>Live Market Dashboard</h2>
+
+      <div>NIFTY : Loading...</div>
+      <div>BANKNIFTY : Loading...</div>
+
+      <br/>
+
+      <button>
+        Connect Angel One
+      </button>
+    </main>
+  )
+}
