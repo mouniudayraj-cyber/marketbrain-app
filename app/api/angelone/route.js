6bbb09f8de5c4002d5b87e522e28ca2c0506febd
mouniudayraj-cyber <mouniudayraj@gmail.com>
2026-06-14 14:01:12 +0530

    Update route.js

--- app/api/angelone/route.js
+++ app/api/angelone/route.js
@@ -1,7 +1,7 @@
 export async function GET() {
   return Response.json({
-    apiKeyPresent: !!process.env.ANGEL_API_KEY,
-    clientCodePresent: !!process.env.ANGEL_CLIENT_CODE,
-    apiSecretPresent: !!process.env.ANGEL_API_SECRET,
+    apiKeyPresent: !!process.env.983mzMi0,
+    clientCodePresent: !!process.env.U19790,
+    apiSecretPresent: !!process.env.O564FFAO2BCQ7CAW5MSKOXWC4A,
   });
 }
